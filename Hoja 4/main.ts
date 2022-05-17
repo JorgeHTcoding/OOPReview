410e179ffa8f838ca5e7c763cf5bfc5eb97e83cc
JorgeHTcoding <105207908+JorgeHTcoding@users.noreply.github.com>
2022-05-18 00:23:55 +0200

    Update main.ts

--- Hoja 4/main.ts
+++ Hoja 4/main.ts
@@ -1,9 +1,9 @@
 import { Punto } from "./reto1";
 import { Triangulo } from "./reto13";
 
-let punto1 = new Punto(1,5)
-let punto2 = new Punto(2,8)
-let punto3 = new Punto(1,-5)
+let punto1 = new Punto(-15,-2)
+let punto2 = new Punto(-15,-6)
+let punto3 = new Punto(25,2)
 let punto4= new Punto(15,5)
 
 let arrayPuntos:Punto[] = [punto1,punto2,punto3]
@@ -18,7 +18,7 @@ let triangulo = new Triangulo(punto4,punto2,punto3)
 // console.log(punto1.calcularCuadrante());
 
 // console.log(arrayPuntos)
-// console.log(punto4.calcularMasCercano(arrayPuntos))
-console.log(triangulo.calcularLongitudLados())
+console.log(punto4.calcularMasCercano(arrayPuntos))
+//console.log(triangulo.calcularLongitudLados())
 
 
